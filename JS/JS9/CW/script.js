3214f0fe5f003e5d31a6170e85cf65d84c5e7556
AlyonaShy <slobalyona@gmail.com>
2023-04-23 22:10:46 +0300

    9cw

---
+++ JS/JS9/CW/script.js
@@ -0,0 +1,42 @@
+let min = document.querySelector("#min"),
+    sec = document.querySelector("#sec"),
+    mSec = document.querySelector("#msec"),
+    start = document.querySelector("#start"),
+    stopButton = document.querySelector("#stop"),
+    restart = document.querySelector("#restart"),
+    counter = 0,
+    intervalHandler;
+
+min.innerHTML = 0;
+sec.innerHTML = 0;
+mSec.innerHTML = 0;
+
+
+let count = () => {
+    counter++;
+    if (counter < 10) {
+        mSec.innerHTML = counter;
+    } else {
+        counter = 0;
+        mSec.innerHTML = counter;
+        sec.innerHTML++;
+        if (sec.innerHTML > 59) {
+            sec.innerHTML = 0;
+            min.innerHTML++;
+        }
+    }
+
+}
+start.onclick = () => {
+    intervalHandler = setInterval(count, 100);
+};
+stopButton.onclick = () => {
+    clearInterval(intervalHandler);
+};
+restart.onclick = () => {
+    clearInterval(intervalHandler);
+    min.innerHTML = 0;
+    sec.innerHTML = 0;
+    mSec.innerHTML = 0;
+    intervalHandler = setInterval(count, 100);
+}
