b8b4eb259dcc75cac91f9c87fa0ab66a9f70ee9e
AlyonaShy <slobalyona@gmail.com>
2023-04-23 23:20:38 +0300

    9cw2

--- JS/JS9/CW/script.js
+++ JS/JS9/CW/script.js
@@ -1,3 +1,17 @@
+document.getElementById("body").style.backgroundColor = "rgb(215, 228, 224)";
+document.querySelector(".container").style = ("max-width: 800px; margin: 0 auto;");
+document.querySelector(".timer").style = ("padding-top: 50px; display: flex; justify-content: center; gap: 15px;");
+document.querySelector(".timer__area1").style = ("width: 200px; height: 100px; background-color: rgb(173, 211, 199); border: 1px solid gray; box-shadow: 5px 5px 10px gray; border-radius: 10px; color: rgb(87, 87, 87); font-size: 55px; font-family: monospace; text-align: center; padding-top: 15px; box-sizing: border-box;");
+document.querySelector(".timer__area2").style = ("width: 200px; height: 100px; background-color: rgb(173, 211, 199); border: 1px solid gray; box-shadow: 5px 5px 10px gray; border-radius: 10px; color: rgb(87, 87, 87); font-size: 55px; font-family: monospace; text-align: center; padding-top: 15px; box-sizing: border-box;");
+document.querySelector(".timer__area3").style = ("width: 200px; height: 100px; background-color: rgb(173, 211, 199); border: 1px solid gray; box-shadow: 5px 5px 10px gray; border-radius: 10px; color: rgb(87, 87, 87); font-size: 55px; font-family: monospace; text-align: center; padding-top: 15px; box-sizing: border-box;");
+document.querySelector(".btns").style = ("padding-top: 50px; display: flex; justify-content: center; gap: 15px;");
+
+let input = document.querySelectorAll("input");
+for(i = 0; i < input.length; i++) {
+    input[i].style = ("width: 100px; height: 40px; background-color: rgb(54, 75, 68); border: none; box-shadow: 5px 5px 10px black; border-radius: 50px; color: #fff; text-transform: uppercase; font-family: monospace;");
+};
+
+
 let min = document.querySelector("#min"),
     sec = document.querySelector("#sec"),
     mSec = document.querySelector("#msec"),
